8955e483671ce3c0f9328be646e92dc16c626d4f
Michał Piaskowski <piaskowski3456@gmail.com>
2021-10-25 16:56:41 +0200

    Dodanie znikania i pokazywania obazków essa

--- app/src/main/java/com/example/galeria/FirstFragment.kt
+++ app/src/main/java/com/example/galeria/FirstFragment.kt
@@ -10,16 +10,18 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.example.galeria.databinding.FragmentFirstBinding
+import android.provider.MediaStore
+
+import android.content.Intent
+
+
+
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
     var licznik = 1
-    val jeden = "jesteś na pierwszym zdjęciu"
-    val cztery = "jesteś na ostatnim zdjęciu"
-    val tJeden = Toast.makeText(activity,jeden,Toast.LENGTH_SHORT)
-    val tCztery = Toast.makeText(activity,cztery,Toast.LENGTH_SHORT)
     private var _binding: FragmentFirstBinding? = null
 
     // This property is only valid between onCreateView and
@@ -32,24 +34,30 @@ class FirstFragment : Fragment() {
     ): View? {
 
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        binding.checkBox.setOnClickListener {
+            if(binding.checkBox.isChecked()){
+                binding.imageView.setVisibility(View.INVISIBLE)
+            }else{
+                binding.imageView.setVisibility(View.VISIBLE)
+            }
+        }
+        binding.ibKamera.setOnClickListener {
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        }
 
         binding.ibPrawo.setOnClickListener{
-            if(licznik==4){
-                tCztery.show()
-            }else{
+
             licznik++
             when(licznik) {
                 1 -> binding.imageView.setImageResource(R.drawable.bialy)
                 2 -> binding.imageView.setImageResource(R.drawable.zolty)
                 3 -> binding.imageView.setImageResource(R.drawable.zloty)
                 4 -> binding.imageView.setImageResource(R.drawable.karmazyn)
-            }
+
             }
         }
         binding.ibLewo.setOnClickListener{
-            if(licznik==1){
-            tJeden.show()
-            }else{
+
             licznik--
             when(licznik) {
                 1 -> binding.imageView.setImageResource(R.drawable.bialy)
@@ -57,7 +65,7 @@ class FirstFragment : Fragment() {
                 3 -> binding.imageView.setImageResource(R.drawable.zloty)
                 4 -> binding.imageView.setImageResource(R.drawable.karmazyn)
             }
-            }
+
         }
 
 
